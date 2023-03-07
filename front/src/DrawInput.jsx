f969a2056c4c2dba0d6d1eb12f912af3f1d5422a
Neven DREAN <nevendrean@yahoo.fr>
2023-03-07 09:46:04 -0500

    css hover

--- front/src/DrawInput.jsx
+++ front/src/DrawInput.jsx
@@ -3,7 +3,14 @@ import React from "react";
 export default function DrawInput({ name, type, val, handleBox, isChecked }) {
   return (
     <div>
-      <label htmlFor={val} className="hover:bg-sky-200 p-1 rounded m-1">
+      <label
+        htmlFor={val}
+        className={
+          name === "Framework"
+            ? "hover:bg-sky-200 p-1 rounded m-1"
+            : "hover:bg-green-200 p-1 rounded m-1"
+        }
+      >
         {val}
         <input
           type={type}
