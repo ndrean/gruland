a3148d080e2e7065850cb454477b7773a5d53af4
Neven DREAN <nevendrean@yahoo.fr>
2023-03-09 22:15:14 -0500

    some CSS

--- front/src/Reset.jsx
+++ front/src/Reset.jsx
@@ -2,7 +2,7 @@ export default function Reset({ handleReset }) {
   return (
     <button
       type="button"
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      className="bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       onClick={handleReset}
     >
       Reset
