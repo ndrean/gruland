cd1429923621961e93122229aca81a31716a03ce
Neven DREAN <nevendrean@yahoo.fr>
2023-03-14 08:53:24 -0500

    refactor builder

--- front/src/github.js
+++ front/src/github.js
@@ -5,7 +5,8 @@ async function getJson(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    return data.filter((l) => Object.keys(l).includes("title"));
+    // return data.filter((l) => Object.keys(l).includes("title"));
+    return data;
   } catch (err) {
     console.log(err);
   }
--- lib/builder.ex
+++ lib/builder.ex
@@ -3,15 +3,22 @@ defmodule Builder do
     Path.wildcard("../serverless-patterns/*/example-*.json")
     |> Stream.map(&File.read!/1)
     |> Stream.map(fn chunk ->
-      case Poison.decode(chunk) do
-        {:ok, json} ->
-          Map.put_new(json, :id, UUID.uuid1())
+      with {:ok, json} <- Poison.decode(chunk) do
+        case Map.has_key?(json, "title") do
+          true ->
+            Map.put_new(json, :id, UUID.uuid1())
 
+          false ->
+            nil
+        end
+      else
         {:error, _} ->
           nil
       end
     end)
-    |> Stream.reject(fn chunk -> chunk == nil end)
+    |> Stream.reject(fn chunk ->
+      chunk == nil
+    end)
     |> Poison.encode!(%{pretty: true, indent: 2})
     |> then(&File.write!("serverlessland-examples.json", &1))
   end
